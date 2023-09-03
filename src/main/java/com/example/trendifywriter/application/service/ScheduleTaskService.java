863b53a5e6410c509fcb49ca9e056b267005716a
jay <joswxx@gmail.com>
2023-09-04 00:39:39 +0900

    데이터시각화

--- src/main/java/com/example/trendifywriter/application/service/ScheduleTaskService.java
+++ src/main/java/com/example/trendifywriter/application/service/ScheduleTaskService.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ScheduleTaskService {
 
+    private final SimpMessagingTemplate simpMessagingTemplate;
     private final FrequencyTrendAnalyzer frequencyTrendAnalyzer;
     private final ApplicationEventPublisher eventPublisher;
     private final RedisTemplate redisTemplate;
@@ -24,7 +26,7 @@ public class ScheduleTaskService {
 
     // 이벤트 - 발행 구독 모델사용  컨트롤러 / 서비스 분리
 
-    @Scheduled(fixedRate = 300000)  // 5분마다 실행
+    @Scheduled(fixedRate = 20000)  // 5분마다 실행
     public void updateTenMinutes() {
         Map<String, Integer> latestKeywords = frequencyTrendAnalyzer.analyze();
         eventPublisher.publishEvent(new KeywordsEvent(latestKeywords)); // 실시간(10분간격) 키워드 웹소켓 업데이트.
@@ -40,17 +42,26 @@ public class ScheduleTaskService {
     // 시간 바뀌기 1초전에 getCurrentHour 을 해야 이전 HH 인 hourly:13 을불러서 넣을수 있다.  0분에 실행하면 안될듯.
     // calculateTopKeywords 메서드 구현필요 -> 탑 10 키워드 정렬 추출.
     // 시간별 데이터 시각화 어떤식으로 프론트에 구현할지 ? , redis말고 rdb는 필요없을지 ?
-    @Scheduled(cron = "0 0 * * * *")  // 매시간 0분에 실행
+//    @Scheduled(cron = "0 0 * * * *")  // 매시간 0분에 실행
+    @Scheduled(fixedRate = 40000)
     public void updateHourly() {
 
         String currentHour = getCurrentHour();
+        System.out.println("currentHour = " + currentHour);
         Map<Object, Object> hourlyData = redisTemplate.opsForHash().entries("hourly:" + currentHour);
 
         redisTemplate.delete("hourly:" + currentHour);
 
         Map<String, Integer> hourlyTopKeywords = calculateTopKeywords(hourlyData);
 
-        eventPublisher.publishEvent(new HourlyKeywordsEvent(hourlyTopKeywords));
+        Map<String, Object> payload = new HashMap<>();
+
+        payload.put("hour", currentHour);
+        payload.put("data", hourlyTopKeywords);
+
+        simpMessagingTemplate.convertAndSend("/topic/hourly_data", payload);
+
+
 
         redisTemplate.opsForHash().putAll("daily:" + currentHour, hourlyTopKeywords);
     }
@@ -59,7 +70,7 @@ public class ScheduleTaskService {
     //  모든 daily 값 가져와서 , calculateTopKeywords 로 추출 .
     // 일별 데이터와 시간별데이터는 , rdb에 저장후 , 데이터시각화에 보여줘야할듯 ? ,
     // 10분데이터는 실시간 키워드 보여줄때만 같이 보여주고  ,rdb에 저장은 안하고 시간별데이터 집게할떄 redis에서 삭제 , 즉 1시간동안 10분데이터를 6번 히스토리 까지만 보여주고 , 삭제 !
-    @Scheduled(cron = "0 0 0 * * *")  // 매일 자정에 실행
+//    @Scheduled(cron = "0 0 0 * * *")  // 매일 자정에 실행
     public void updateDaily() {
 
         Map<Object, Object> daily = redisTemplate.opsForHash().entries("daily");
